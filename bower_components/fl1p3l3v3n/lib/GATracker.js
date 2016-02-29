6490c84f1f7abe69eea6b8515f7e0074f2ef7f7e
Tony DiPerna <dipernaa@msoe.edu>
2016-02-29 10:39:55 -0600

    first commit

---
+++ bower_components/fl1p3l3v3n/lib/GATracker.js
@@ -0,0 +1,12 @@
+Class(function GATracker() {
+    this.trackPage = function (page) {
+        if (typeof ga !== "undefined") {
+            ga("send", "pageview", page);
+        }
+    };
+    this.trackEvent = function (b, d, a, c) {
+        if (typeof ga !== "undefined") {
+            ga("send", "event", b, d, a, (c || 0));
+        }
+    };
+}, 'static');
