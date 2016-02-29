8cdbc4e2f60c96da8358509b24e075b46837749f
Tony DiPerna <ryan_harrison_73@yahoo.com>
2016-02-29 11:32:57 -0600

    Commit Test with new key pt3

--- bower_components/fl1p3l3v3n/sample-project/js/site/Controllers/Home.js
+++ bower_components/fl1p3l3v3n/sample-project/js/site/Controllers/Home.js
@@ -40,7 +40,6 @@ Class(function Home() {
     function _events() {
         Evt.subscribe(window, Evt.RESIZE, _onResize);
         // _button.interact(_onOver, _onOut, _onClick);
-        //Nothing
     }
 
     function _onOver(){
